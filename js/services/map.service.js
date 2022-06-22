d1fcc615c546e9fb0ed834a962fda03e658e406c
Shai <shay4447@gmail.com>
2022-06-22 16:49:57 +0300

    .

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -22,7 +22,7 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
                 center: { lat, lng },
                 zoom: 15
             })
-            console.log('Map!', gMap);
+            // console.log('Map!', gMap);
         })
 }
 
